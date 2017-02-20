5d3b0733856754c4ee114b8d785144b084e2e71b
SmartDodging <hellabeatz666@gmail.com>
2017-02-20 15:54:06 +0100

    edit login

--- login.php
+++ login.php
@@ -1,16 +1,17 @@
 <?php include("nav.php");?>
 <center>
+    <h1>Log In!</h1>
     <div class="container">
-            <form action="" role="form" class="form-inline">
+        <form action="" role="form" class="form-inline">
             <label for="form-el-email">Username</label>
-                <br>
+            <br>
             <input type="email" id="form-el-email" class="form-control" placeholder="Username">
-            </form>
-            <form action="" role="form" class="form-inline">
+        </form>
+        <form action="" role="form" class="form-inline">
             <label for="form-el-password">Password</label>
-                <br>
+            <br>
             <input type="password" id="form-el-password" class="form-control" placeholder="Password">
-            </form>
-            <button class="btn btn-danger" type="button"><span class="glyphicon glyphicon-ok"></span>   login</button>
-        </div>
+        </form>
+        <button class="btn btn-danger" type="button"><span class="glyphicon glyphicon-ok"></span>   login</button>
+    </div>
 </center>
