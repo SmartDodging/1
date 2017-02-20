a713a32834d0d60e348a0dd69ef624838a1716b2
Jop Laane <joplaane@gmail.com>
2017-02-20 15:32:41 +0100

    edit signup

--- signup.php
+++ signup.php
@@ -4,19 +4,19 @@
         <div class="demo">
             <div class="container">
                 <form action="" role="form" class="form-inline">
-                    <label for="form-el-email">E-mail</label><br>
+                    <label for="form-el-email">E-mail*</label><br>
                     <input type="email" id="form-el-email" class="form-control" placeholder="E-mail" required> 
                 </form>
                 <form action="" role="form" class="form-inline">
-                    <label for="form-el-username">Username</label><br>
+                    <label for="form-el-username">Username*</label><br>
                     <input type="text" id="form-el-username" class="form-control" placeholder="Username" required>
                 </form>
                 <form action="" role="form" class="form-inline">
-                    <label for="form-el-password">Password</label><br>
+                    <label for="form-el-password">Password*</label><br>
                     <input type="password" id="form-el-password" class="form-control" placeholder="Password" required> 
                 </form>
                 <form action="" role="form" class="form-inline">
-                    <label for="form-el-password">Repeat password</label><br>
+                    <label for="form-el-password">Repeat password*</label><br>
                     <input type="password" id="form-el-password" class="form-control" placeholder="Password" required> 
                 </form>
                 <br>
