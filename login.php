4d87177bf798d9b28215bccd91d8776dc93fb1f4
SmartDodging <hellabeatz666@gmail.com>
2017-02-20 15:42:49 +0100

    edit login

--- login.php
+++ login.php
@@ -1,28 +1,16 @@
 <?php include("nav.php");?>
 <center>
     <div class="container">
-            <form action="" role="form">
-                  <div class="form-group">
-                      <label for="form-el-achternaam">Achternaam</label>
-                      <input type="text" id="form-el-achternaam" class="form-control">
-                </div>   
-            </form>
-            <form action="" role="form">
-                  <div class="form-group">
-                      <label for="form-el-voornaam">Voornaam</label>
-                      <input type="text" id="form-el-voornaam" class="form-control">
-                      <p class="help-block" style="color:red;">Een voornaam is genoeg</p>
-                </div>   
-            </form>
-            <hr><hr>
             <form action="" role="form" class="form-inline">
-            <label for="form-el-email" class="sr-only">E-mail</label>
-            <input type="email" id="form-el-email" class="form-control" placeholder="Uw email">
+            <label for="form-el-email">Username</label>
+                <br>
+            <input type="email" id="form-el-email" class="form-control" placeholder="Username">
             </form>
             <form action="" role="form" class="form-inline">
-            <label for="form-el-password" class="sr-only">Wachtwoord</label>
-            <input type="password" id="form-el-password" class="form-control" placeholder="Uw wachtwoord">
+            <label for="form-el-password">Password</label>
+                <br>
+            <input type="password" id="form-el-password" class="form-control" placeholder="Password">
             </form>
-            <button class="btn btn-danger" type="button"><span class="glyphicon glyphicon-ok"></span></button>
+            <button class="btn btn-danger" type="button"><span class="glyphicon glyphicon-ok"></span>   login</button>
         </div>
 </center>
