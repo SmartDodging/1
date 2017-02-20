b78fde707be0a8aaaccca99dfa6223df1c25479d
Jop Laane <joplaane@gmail.com>
2017-02-20 15:27:39 +0100

    Added signup

--- signup.php
+++ signup.php
@@ -1,1 +1,27 @@
-<?php include('nav.php');?>
+<?php include 'nav.php'; ?>
+<center>
+    <h1>Sign Up!</h1>
+        <div class="demo">
+            <div class="container">
+                <form action="" role="form" class="form-inline">
+                    <label for="form-el-email">E-mail</label><br>
+                    <input type="email" id="form-el-email" class="form-control" placeholder="E-mail" required> 
+                </form>
+                <form action="" role="form" class="form-inline">
+                    <label for="form-el-username">Username</label><br>
+                    <input type="text" id="form-el-username" class="form-control" placeholder="Username" required>
+                </form>
+                <form action="" role="form" class="form-inline">
+                    <label for="form-el-password">Password</label><br>
+                    <input type="password" id="form-el-password" class="form-control" placeholder="Password" required> 
+                </form>
+                <form action="" role="form" class="form-inline">
+                    <label for="form-el-password">Repeat password</label><br>
+                    <input type="password" id="form-el-password" class="form-control" placeholder="Password" required> 
+                </form>
+                <br>
+                <button class="btn btn-success" type="submit" style="color:red;"><span class="glyphicon glyphicon-heart"></span></button>
+            </div>
+        </div>
+</center>
+                    <!--<p class="help-block" style="color:red;">Een acternaam is genoeg</p>-->
