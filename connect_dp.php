9f424cb62fdd4519f9d7f5dc597466094237fda1
SmartDodging <hellabeatz666@gmail.com>
2017-02-24 13:47:24 +0100

    Delete connect_dp.php

--- connect_dp.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "	mysql.hostinger.nl";
-$username = "u155808843_dodge";
-$password = "kaas123";
-$dbname = "u155808843_data";
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-?>
