63a31e773d21562a680a4e6753cc305a8fc8137b
SmartDodging <hellabeatz666@gmail.com>
2017-03-13 14:38:49 +0100

    ..

--- news.php
+++ news.php
@@ -25,55 +25,54 @@
       <!-- Left and right controls -->
       <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
       </a>
       <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
       </a>
     </div>
 </center>
 
-
-<!--news thingies-->
-<div class="container">
-    <div id="blog" class="row">
-        <div class="col-md-10 blogShort">
-            <h1>News</h1>
-                <img src="css/IMG/21601-news-logo-icon-vector-icon-vector-eps.png" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
-                <!--<em>This snippet use <a href="http://bootsnipp.com/snippets/featured/sexy-sidebar-navigation" target="_blank">Sexy Sidebar Navigation</a></em>-->
-                    <article><p>
-                        this is an update about the game and its great.<br>
-                        this is an update about the game and its great.<br>
-                        this is an update about the game and its great.<br>
-                        this is an update about the game and its great.
-                    </p></article>
-                <a class="btn btn-blog pull-right marginBottom10" href="#">READ MORE</a> 
-        </div>
-        <div class="col-md-10 blogShort">
-            <h1>News</h1>
-                <img src="css/IMG/21601-news-logo-icon-vector-icon-vector-eps.png" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
-                    <article><p>
-                        this is an update about the game and its great.<br>
-                        this is an update about the game and its great.<br>
-                        this is an update about the game and its great.<br>
-                        this is an update about the game and its great.
-                    </p></article>
-                <a class="btn btn-blog pull-right marginBottom10" href="#">READ MORE</a> 
-        </div>  
-        <div class="col-md-10 blogShort">
-            <h1>News</h1>
-                <img src="css/IMG/21601-news-logo-icon-vector-icon-vector-eps.png" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
-                    <article><p>
-                        this is an update about the game and its great.<br>
-                        this is an update about the game and its great.<br>
-                        this is an update about the game and its great.<br>
-                        this is an update about the game and its great.
-                    </p></article>
-                <a class="btn btn-blog pull-right marginBottom10" href="#">READ MORE</a> 
+<center>
+    <!--news thingies-->
+    <div class="container">
+        <div id="blog" class="row">
+            <div class="col-md-10 blogShort">
+                <h1>News</h1>
+                    <img src="css/IMG/21601-news-logo-icon-vector-icon-vector-eps.png" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
+                    <!--<em>This snippet use <a href="http://bootsnipp.com/snippets/featured/sexy-sidebar-navigation" target="_blank">Sexy Sidebar Navigation</a></em>-->
+                        <article><p>
+                            this is an update about the game and its great.<br>
+                            this is an update about the game and its great.<br>
+                            this is an update about the game and its great.<br>
+                            this is an update about the game and its great.
+                        </p></article>
+                    <a class="btn btn-blog pull-right marginBottom10" href="#">READ MORE</a> 
+            </div>
+            <div class="col-md-10 blogShort">
+                <h1>News</h1>
+                    <img src="css/IMG/21601-news-logo-icon-vector-icon-vector-eps.png" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
+                        <article><p>
+                            this is an update about the game and its great.<br>
+                            this is an update about the game and its great.<br>
+                            this is an update about the game and its great.<br>
+                            this is an update about the game and its great.
+                        </p></article>
+                    <a class="btn btn-blog pull-right marginBottom10" href="#">READ MORE</a> 
+            </div>  
+            <div class="col-md-10 blogShort">
+                <h1>News</h1>
+                    <img src="css/IMG/21601-news-logo-icon-vector-icon-vector-eps.png" alt="post img" class="pull-left img-responsive thumb margin10 img-thumbnail">
+                        <article><p>
+                            this is an update about the game and its great.<br>
+                            this is an update about the game and its great.<br>
+                            this is an update about the game and its great.<br>
+                            this is an update about the game and its great.
+                        </p></article>
+                    <a class="btn btn-blog pull-right marginBottom10" href="#">READ MORE</a> 
+            </div>
         </div>
     </div>
-</div>
+</center>
 
 <footer>
 pens
