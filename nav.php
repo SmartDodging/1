ff1081b29f289c06233c1bf291cfcf668877c008
SmartDodging <hellabeatz666@gmail.com>
2017-03-16 11:19:31 +0100

    Pictures added

--- nav.php
+++ nav.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">        
         <meta http-equiv="X-UA-Compatible" content="IE=edge">       
         <meta name="viewport" content="width=device-width, initial-scale=1">      
-        <link rel="icon" href="http://example.com/favicon.png">
+        <link rel="icon" href="css/IMG/dodging.png">
         <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">       
         <title>Smart Dodging</title><br>   
