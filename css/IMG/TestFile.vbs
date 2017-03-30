cf5c12b0fe083cf935e94540db23af73b21ae542
SmartDodging <hellabeatz666@gmail.com>
2017-03-30 10:19:52 +0200

    DOWNLOAAAAAAAAAAAAAAAAAAAD

---
+++ css/IMG/TestFile.vbs
@@ -0,0 +1,2 @@
+msg=Msgbox("This is just a test file...",vbIgnore+vbExclamation, "Fataaaa")
+WScript.Echo msg
--- download.php
+++ download.php
@@ -2,10 +2,10 @@
 
 <center>    
     <h1>Download The Game!</h1>
-    <br>    
-    <button class="btn btn-success" type="button" id="downloadbtn">Download Game!</button>  
+    <br>     
+    <button class="btn btn-success" id="downloadbtn"><a href = "css/IMG/001-subtle-light-pattern-background-texture-vol5.jpg" download="Hacked" style="text-decoration:none;color:firebrick;">Download!</a></button>
     <br><br>
-    <button class="btn btn-link" type="button" id="downloadMac">Download For Mac!</button>
-    <button class="btn btn-link" type="button" id="downloadLin">Download For Linux!</button>
+    <button class="btn btn-link" type="button" id="downloadMac"><a href="css/IMG/background-tile-2e75a594a803c883d5eb4ca740e9021d.png" download="Hacked">Download For Mac!</a></button>
+    <button class="btn btn-link" type="button" id="downloadLin"><a href="css/IMG/download.jpg" download="Hacked">Download For Linux!</a></button>
     <hr>
 </center>
