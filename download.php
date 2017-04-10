5dbfc3dc2e4c23482028769959e4c3b15da18828
SmartDodging <hellabeatz666@gmail.com>
2017-04-10 13:39:30 +0200

    Modal
    I fixed the modal, and fixed the download, will make it download a .exe
    soon

--- download.php
+++ download.php
@@ -3,7 +3,7 @@
 <center>    
     <h1>Download The Game!</h1>
     <br>     
-    <button class="btn btn-success" id="downloadbtn"><a href = "css/IMG/001-subtle-light-pattern-background-texture-vol5.jpg" download="Hacked" style="text-decoration:none;color:firebrick;">Download!</a></button>
+    <button class="btn btn-success" id="downloadbtn"><a href = "css/IMG/background-tile-2e75a594a803c883d5eb4ca740e9021d.png" download="Hacked" style="text-decoration:none;color:firebrick;">Download!</a></button>
     <br><br>
     <button class="btn btn-link" type="button" id="downloadMac"><a href="css/IMG/background-tile-2e75a594a803c883d5eb4ca740e9021d.png" download="Hacked">Download For Mac!</a></button>
     <button class="btn btn-link" type="button" id="downloadLin"><a href="css/IMG/download.jpg" download="Hacked">Download For Linux!</a></button>
